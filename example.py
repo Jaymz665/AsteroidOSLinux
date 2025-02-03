a85e85f9ab30dbc643ef09e93f27ce0183105cda
Jartz <54475558+Jaymz665@users.noreply.github.com>
2025-02-03 15:17:43 +0300

    Add files via upload

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import argparse
 import asteroid
 import asteroid.app
 from asteroid.module import *
+from asteroid.wt import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -19,16 +20,17 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-ADDRESS = "43:43:A0:12:1F:AC"
-OWM_KEY = "XXXXXXXXXXXXXXXXXXXXXXXX"
-OWM_LOCATION = "Prague"
+ADDRESS = "xx:xx:xx:xx:xx:xx"
+API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXX'
+CITY_NAME = 'Moscow'
+
 
 app = asteroid.app.App(ADDRESS, verbose=args.verbose)
 
 app.register_module(ReconnectModule(timeout_base=10))
 app.register_module(TimeSyncModule())
 app.register_module(NotifyModule())
-app.register_module(OWMModule(api_key=OWM_KEY, location=OWM_LOCATION))
+app.register_module(OWMModule(api_key=API_KEY, city=CITY_NAME))
 app.register_module(MPDModule())
 
 if args.interactive:
